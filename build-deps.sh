b4fba10fcbe30f1f568c8e83de175b6b67037ec8
Adrian Lam <75461199+adrian-8901@users.noreply.github.com>
2021-06-07 18:04:22 +0800

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,7 +5,7 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-./sync.sh https://github.com/mTresk/android_kernel_oneplus_msm8998.git "data/kernel" "${REF}" &
+./sync.sh https://github.com/Official-Ayrton990/android_kernel_xiaomi_sm8250.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
 ./sync.sh https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "data/gcc" &
 PIDS="${PIDS} $!"
